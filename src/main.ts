606155222f5eb563b0723e50ec774a1d242fa2d6
wesleyara <wes1gom@gmail.com>
2026-01-31 11:48:49 -0300

    feat: create encode and decode routes

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log("Hello, Base64 API with GitHub CI!");
+import express from "express";
+
+import { router } from "./routes/index.js";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.get("/api/v1", (req, res) => {
+  res.send("Welcome to the Base64 API!");
+});
+
+app.use("/api/v1/base64", router);
+
+app.listen(port, () => {
+  console.log(`Base64 API server running at http://localhost:${port}`);
+});
